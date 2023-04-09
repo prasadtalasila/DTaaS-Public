26872b5330066289a53556913d8bfaa94a269f69
prasadtalasila <prasad.talasila@gmail.com>
2023-04-09 22:51:43 +0200

    fixes more issues

--- script/install.bash
+++ script/install.bash
@@ -16,8 +16,8 @@ apt-get install -y \
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 printf \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=%s signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  %s stable" $(dpkg --print-architecture) $(lsb_release -cs)  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 groupadd docker
