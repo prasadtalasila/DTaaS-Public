fa8d2fbdd86547d17418eff89e358b2849fb00ab
prasadtalasila <prasad.talasila@gmail.com>
2023-06-15 20:36:56 +0200

    remove unused scripts

---
+++ script/docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export commit_hash=$(git rev-parse --short HEAD)
+
+printf "generate and publish documents"
+mkdocs build --config-file mkdocs.yml --site-dir site/online/latest
+mkdocs build --config-file mkdocs_offline.yml --site-dir site/offline/latest
+cd site/offline
+zip -r latest.zip latest
+cp docs/redirect-page.html site/index.html
+
+git checkout website-docs
+
+rm -rf latest
+mv site/online/latest . 
+mv site/online/latest.zip .
+
+
+git add .
+git commit -m "docs for ${commit_hash} commit"
