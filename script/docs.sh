d70e86f1ef9abfadc7209354d86084ff2f303b43
prasadtalasila <prasad.talasila@gmail.com>
2023-06-15 20:47:12 +0200

    improves docs script

--- script/docs.sh
+++ script/docs.sh
@@ -13,8 +13,8 @@ git checkout website-docs
 
 rm -rf latest
 mv site/online/latest . 
-mv site/online/latest.zip .
-
+mv site/offline/latest.zip .
+mv site/index.html .
 
 git add .
 git commit -m "docs for ${commit_hash} commit"
