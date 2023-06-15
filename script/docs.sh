7d95ee67b9576e5655bd0b2316a933b5e3e8dc86
prasadtalasila <prasad.talasila@gmail.com>
2023-06-15 21:04:06 +0200

    improves docs script

--- script/docs.sh
+++ script/docs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export VERSION={"$1":-latest}
 export TOP_DIR=$(pwd)
 export COMMIT_HASH=$(git rev-parse --short HEAD)
 
+echo ${VERSION}
 printf "generate and publish documents"
-mkdocs build --config-file mkdocs.yml --site-dir site/online/latest
-mkdocs build --config-file mkdocs_offline.yml --site-dir site/offline/latest
+mkdocs build --config-file mkdocs.yml --site-dir "site/online/${VERSION}"
+mkdocs build --config-file mkdocs_offline.yml --site-dir "site/offline/${VERSION}"
 
 cp docs/redirect-page.html site/index.html
 
@@ -13,7 +15,7 @@ cd site/offline
 zip -r latest.zip latest
 
 cd ${TOP_DIR}
-git checkout website-docs
+git checkout webpage-docs
 
 rm -rf latest
 mv site/online/latest . 
